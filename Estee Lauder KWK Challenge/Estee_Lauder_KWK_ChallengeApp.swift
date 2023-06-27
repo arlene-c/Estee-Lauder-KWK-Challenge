25be49432ad3969ed560664c30bb1beb485009fd
arlene <arlene2k7@gmail.com>
2023-06-26 21:14:47 -0700

    Initial Commit

---
+++ Estee Lauder KWK Challenge/Estee_Lauder_KWK_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Estee_Lauder_KWK_ChallengeApp.swift
+//  Estee Lauder KWK Challenge
+//
+//  Created by Arlene Chang on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Estee_Lauder_KWK_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
